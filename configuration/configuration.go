f56a8560a2a5f630379c7a592cbd5fb898522134
Luíz Henrique de Carvalho Teles <rlyowlly19@gmail.com>
2025-06-23 15:48:49 -0300

    configuring pool db

--- configuration/configuration.go
+++ configuration/configuration.go
@@ -17,7 +17,10 @@ type ConfServer struct {
 }
 
 type ConfDB struct {
-	DSN string `env:"GREENLIGHT_DB_DSN,required"`
+	DSN          string `env:"DB_DSN,required"`
+	MaxOpenConns int    `env:"DB_MAX_OPEN_CONNS,required"`
+	MaxIdleConns int    `env:"DB_MAX_IDLE_CONNS,required"`
+	MaxIdleTime  string `env:"DB_MAX_IDLE_TIME,required"`
 }
 
 func New() *Conf {
