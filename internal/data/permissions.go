8e4d8b7473aaccd6780b6e739240998f3f8561f5
Luíz Henrique de Carvalho Teles <rlyowlly19@gmail.com>
2025-08-10 17:17:55 -0300

    on save user set permission

--- internal/data/permissions.go
+++ internal/data/permissions.go
@@ -5,11 +5,25 @@ import (
 	"database/sql"
 	"slices"
 	"time"
+
+	"github.com/lib/pq"
 )
 
 type Permissions []string
 type PermissionModel struct {
 	DB *sql.DB
+}
+
+func (m PermissionModel) AddForUser(userID int64, codes ...string) error {
+	query := `
+	INSERT INTO users_permissions
+	SELECT $1, permissions.id FROM permissions WHERE permissions.code = ANY($2)`
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	_, err := m.DB.ExecContext(ctx, query, userID, pq.Array(codes))
+	return err
 }
 
 func (p Permissions) Include(code string) bool {
