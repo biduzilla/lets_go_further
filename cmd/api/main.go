ecc54d928fae18ce7cd2228532fc0ba6c4c140ee
Luíz Henrique de Carvalho Teles <rlyowlly19@gmail.com>
2025-08-13 10:11:10 -0300

    cors

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -9,6 +9,7 @@ import (
 	"greenlight/internal/jsonlog"
 	"greenlight/internal/mailer"
 	"os"
+	"strings"
 	"sync"
 	"time"
 
@@ -38,6 +39,9 @@ type config struct {
 		password string
 		sender   string
 	}
+	cors struct {
+		trustedOrigins []string
+	}
 }
 
 type application struct {
@@ -65,7 +69,12 @@ func main() {
 	flag.StringVar(&cfg.smtp.username, "smtp-username", c.Mail.USERNAME, "SMTP username")
 	flag.StringVar(&cfg.smtp.password, "smtp-password", c.Mail.PASSWORD, "SMTP password")
 	flag.StringVar(&cfg.smtp.sender, "smtp-sender", "Greenlight <no-reply@greenlight.alexedwards.net>", "SMTP sender")
-	flag.Parse()
+
+	flag.Func("cors-trusted-origins", "Trusted CORS origins (space separated)", func(val string) error {
+		cfg.cors.trustedOrigins = strings.Fields(val)
+		return nil
+	})
+
 	flag.Parse()
 
 	logger := jsonlog.New(os.Stdout, jsonlog.LevelInfo)
